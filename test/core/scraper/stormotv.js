32f6855226cd77de1f5da17ade00435fd8f2e613
Sébastien Règne <regseb@gmail.com>
2019-09-01 16:14:19 +0200

    test: Actualiser le test de StormoTV.

--- test/core/scraper/stormotv.js
+++ test/core/scraper/stormotv.js
@@ -31,7 +31,7 @@ describe("scraper/stormotv", function () {
             const url = "https://www.stormo.tv/videos/338790" +
                                                        "/tancuyushchaya-zebra/";
             const expected = "https://www.stormo.tv/get_file/24" +
-                                 "/34e334698d06ca878d9842e10fe834340b0ea7f0fc" +
+                                 "/34e334698d06ca878d9842e10fe8343455d522f26a" +
                                                    "/338000/338790/338790.mp4/";
             return action(new URL(url)).then(function (file) {
                 assert.strictEqual(file, expected);
