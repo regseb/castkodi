dedc2de45b85fa3345d4bbb58ff7d57fb9f718b5
Sébastien Règne <regseb@gmail.com>
2020-07-25 14:38:01 +0200

    feat: Enlever Mixer.

--- src/core/scrapers.js
+++ src/core/scrapers.js
@@ -29,7 +29,6 @@ import * as lbry           from "./scraper/lbry.js";
 import * as ldjson         from "./scraper/ldjson.js";
 import * as metacafe       from "./scraper/metacafe.js";
 import * as mixcloud       from "./scraper/mixcloud.js";
-import * as mixer          from "./scraper/mixer.js";
 import * as mycloudplayers from "./scraper/mycloudplayers.js";
 import * as onetv          from "./scraper/onetv.js";
 // eslint-disable-next-line import/no-cycle
@@ -82,7 +81,6 @@ const SCRAPERS = [
     lbry,
     metacafe,
     mixcloud,
-    mixer,
     mycloudplayers,
     onetv,
     ouestfrance,
