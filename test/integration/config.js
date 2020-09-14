ff1ace1bf58dcdb0051560b0ab2da698aa3e8192
Sébastien Règne <regseb@gmail.com>
2020-09-14 19:33:56 +0200

    chore: Mettre à jour les dépendances.

--- test/integration/config.js
+++ test/integration/config.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-process-env */
 
 export const config = {
-    country: "COUNTRY" in process.env ? process.env.COUNTRY
-                                      : null,
+    country: process.env.COUNTRY ?? null,
 };
--- test/polyfill/fetch.js
+++ test/polyfill/fetch.js
@@ -4,8 +4,7 @@ const USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0)" +
                    "Gecko/20100101 Firefox/72.0";
 
 export const fetch = function (resource, init = {}) {
-    const headers = "headers" in init ? init.headers
-                                      : {};
+    const headers = init.headers ?? {};
     headers["Accept-Language"] = "*";
     headers["User-Agent"] = USER_AGENT;
     return nodeFetch(resource, { ...init, headers });
