b4b3d28df67f81988f01985df44d779f2f31fc37
Sébastien Règne <regseb@gmail.com>
2020-11-07 16:23:48 +0100

    fix: Actualiser le scraper de Bigo Live.

--- src/core/scraper/bigo.js
+++ src/core/scraper/bigo.js
@@ -6,30 +6,25 @@
 import { matchPattern } from "../../tools/matchpattern.js";
 
 /**
- * L'expression rationnelle pour extraire l'URL de la vidéo.
+ * L'URL de l'API de Bigo Live pour obtenir des informations sur la vidéo.
  *
- * @constant {RegExp}
+ * @constant {string}
  */
-const URL_REGEXP = /videoSrc: "(?<videoSrc>[^"]+)",/u;
+const API_URL = "https://www.bigo.tv/studio/getInternalStudioInfo";
 
 /**
  * Extrait les informations nécessaire pour lire une vidéo sur Kodi.
  *
- * @param {URL}      _url         L'URL d'une vidéo de Bigo Live.
- * @param {object}   content      Le contenu de l'URL.
- * @param {Function} content.html La fonction retournant la promesse contenant
- *                                le document HTML.
+ * @param {URL} url L'URL d'une vidéo de Bigo Live.
  * @returns {Promise.<?string>} Une promesse contenant le lien du
  *                              <em>fichier</em> ou <code>null</code>.
  */
-const action = async function (_url, content) {
-    const doc = await content.html();
-    for (const script of doc.querySelectorAll("script:not([src])")) {
-        const result = URL_REGEXP.exec(script.text);
-        if (null !== result) {
-            return result.groups.videoSrc;
-        }
-    }
-    return null;
+const action = async function ({ pathname }) {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        body:   new URLSearchParams({ siteId: pathname.slice(1) }),
+    });
+    const json = await response.json();
+    return json.data.hls_src ?? null;
 };
 export const extract = matchPattern(action, "*://www.bigo.tv/*");
--- test/unit/core/scraper/bigo.js
+++ test/unit/core/scraper/bigo.js
@@ -1,4 +1,5 @@
 import assert      from "assert";
+import sinon       from "sinon";
 import { extract } from "../../../../src/core/scraper/bigo.js";
 
 describe("core/scraper/bigo.js", function () {
@@ -11,38 +12,42 @@ describe("core/scraper/bigo.js", function () {
         });
 
         it("should return null when it's not a video", async function () {
-            const url = new URL("https://www.bigo.tv/foo");
-            const content = {
-                html: () => Promise.resolve(new DOMParser().parseFromString(`
-                    <html>
-                      <body>
-                        <script></script>
-                      </body>
-                    </html>`, "text/html")),
-            };
+            const stub = sinon.stub(globalThis, "fetch").resolves(new Response(
+                JSON.stringify({ data: [] }),
+            ));
 
-            const file = await extract(url, content);
+            const url = new URL("https://www.bigo.tv/foo");
+
+            const file = await extract(url);
             assert.strictEqual(file, null);
+
+            assert.strictEqual(stub.callCount, 1);
+            assert.deepStrictEqual(stub.firstCall.args, [
+                "https://www.bigo.tv/studio/getInternalStudioInfo",
+                { method: "POST", body: new URLSearchParams("siteId=foo") },
+            ]);
+
+            stub.restore();
         });
 
         it("should return video URL", async function () {
-            const url = new URL("http://www.bigo.tv/foo");
-            const content = {
-                html: () => Promise.resolve(new DOMParser().parseFromString(`
-                    <html>
-                      <body>
-                        <script>
-                          var CONFIG = {
-                              videoSrc: "http://bar.tv/baz.m3u8",
-                              qux: "quux"
-                          }];
-                        </script>
-                      </body>
-                    </html>`, "text/html")),
-            };
+            const stub = sinon.stub(globalThis, "fetch").resolves(new Response(
+                // eslint-disable-next-line camelcase
+                JSON.stringify({ data: { hls_src: "http://bar.tv/baz.m3u8" } }),
+            ));
 
-            const file = await extract(url, content);
+            const url = new URL("http://www.bigo.tv/foo");
+
+            const file = await extract(url);
             assert.strictEqual(file, "http://bar.tv/baz.m3u8");
+
+            assert.strictEqual(stub.callCount, 1);
+            assert.deepStrictEqual(stub.firstCall.args, [
+                "https://www.bigo.tv/studio/getInternalStudioInfo",
+                { method: "POST", body: new URLSearchParams("siteId=foo") },
+            ]);
+
+            stub.restore();
         });
     });
 });
