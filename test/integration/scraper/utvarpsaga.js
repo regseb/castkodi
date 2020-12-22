387684847ad9b418c521ff8cdea3ed9e9925c609
Sébastien Règne <regseb@gmail.com>
2020-12-22 14:09:55 +0100

    test: Actualiser les tests de Utvarp Saga.

--- test/integration/scraper/utvarpsaga.js
+++ test/integration/scraper/utvarpsaga.js
@@ -3,13 +3,24 @@ import { extract } from "../../../src/core/scrapers.js";
 
 describe("Scraper: Útvarp Saga", function () {
     it("should return video URL [audio]", async function () {
-        const url = new URL("https://utvarpsaga.is/" +
-                            "snjallsimarnir-eru-farnir-ad-stjorna-lifi-folks/");
+        const url = new URL("https://www.utvarpsaga.is/snjallsimarnir-eru" +
+                                              "-farnir-ad-stjorna-lifi-folks/");
         const options = { depth: false, incognito: false };
 
         const file = await extract(url, options);
         assert.strictEqual(file,
             "https://www.utvarpsaga.is/file/" +
                                           "s%C3%AD%C3%B0degi-a-7.9.18.mp3?_=1");
+    });
+
+    it("should return video URL without 'www' [audio]", async function () {
+        const url = new URL("https://utvarpsaga.is/samfelagsmidlar-mynda" +
+                                     "-nuning-milli-hopa-med-olikar-skodanir/");
+        const options = { depth: false, incognito: false };
+
+        const file = await extract(url, options);
+        assert.strictEqual(file,
+            "https://www.utvarpsaga.is/file/" +
+                                "vi%C3%B0tal-%C3%ADva-mar%C3%ADn-24.09.20.mp3");
     });
 });
