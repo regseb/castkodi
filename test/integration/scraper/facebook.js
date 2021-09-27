a76620a42f0cea97ef6309c364f2da97af997177
Sébastien Règne <regseb@gmail.com>
2021-09-27 18:10:34 +0200

    test: Désactiver les tests de Facebook et Instagram aux États-Unis.

--- test/integration/scraper/facebook.js
+++ test/integration/scraper/facebook.js
@@ -1,7 +1,15 @@
 import assert from "node:assert";
+import { config } from "../config.js";
 import { extract } from "../../../src/core/scrapers.js";
 
 describe("Scraper: Facebook", function () {
+    before(function () {
+        if (null !== config.country && "fr" !== config.country) {
+            // eslint-disable-next-line no-invalid-this, @babel/no-invalid-this
+            this.skip();
+        }
+    });
+
     it("should return URL when it's not a video", async function () {
         const url = new URL("https://www.facebook.com/XBMC/videos/666/");
         const options = { depth: false, incognito: false };
--- test/integration/scraper/instagram.js
+++ test/integration/scraper/instagram.js
@@ -12,10 +12,7 @@ describe("Scraper: Instagram", function () {
         }
     });
 
-    // Désactiver les tests d'Instagram car ils ne fonctionnent pas dans Node.
-    // La méthode fetch() se connecte en IPV4 à Instagram et les pages semblent
-    // accessible seulement en IPV6.
-    it.skip("should return URL when it's not a video", async function () {
+    it("should return URL when it's not a video", async function () {
         const url = new URL("https://www.instagram.com/p/6p_BDeK-8G/");
         const options = { depth: false, incognito: false };
 
@@ -23,7 +20,7 @@ describe("Scraper: Instagram", function () {
         assert.strictEqual(file, url.href);
     });
 
-    it.skip("should return video URL [opengraph]", async function () {
+    it("should return video URL [opengraph]", async function () {
         const url = new URL("https://www.instagram.com/p/BpFwZ6JnYPq/");
         const options = { depth: false, incognito: false };
 
@@ -32,7 +29,7 @@ describe("Scraper: Instagram", function () {
                   `new URL("${file}").pathname.endsWith(...)`);
     });
 
-    it.skip("should return video URL when protocol is HTTP [opengraph]",
+    it("should return video URL when protocol is HTTP [opengraph]",
                                                              async function () {
         const url = new URL("http://www.instagram.com/p/Bpji87LiJFs/");
         const options = { depth: false, incognito: false };
