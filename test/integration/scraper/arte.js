1aa3c9923d4967f8c036c65c3b8ab95eb647b6b9
Sébastien Règne <regseb@gmail.com>
2022-04-09 10:08:10 +0200

    test: Fiabiliser les tests d'Arte.

--- test/integration/scraper/arte.js
+++ test/integration/scraper/arte.js
@@ -21,16 +21,16 @@ describe("Scraper: Arte", function () {
     });
 
     it("should return french video URL", async function () {
-        // Récupérer l'URL d'une vidéo sur la page d'accueil.
-        const response = await fetch("https://www.arte.tv/fr/");
-        const text = await response.text();
-        const doc = new DOMParser().parseFromString(text, "text/html");
+        // Récupérer l'URL d'une vidéo parmi les vidéos les plus vues.
+        const response = await fetch("https://www.arte.tv/api/rproxy/emac/v3" +
+                                                 "/fr/web/data/VIDEO_LISTING/" +
+                                  "?videoType=MOST_VIEWED&authorizedAreas=ALL");
+        const json = await response.json();
+        // Garder les éléments pointant vers une seule vidéos (et non vers une
+        // liste de vidéos).
+        const video = json.data.find((d) => !d.kind.isCollection);
 
-        // Récupérer le dernier de la liste pour ne pas prendre des vidéos qui
-        // ne sont pas encore disponibles.
-        const url = new URL(
-            doc.querySelector(`[data-testid="teaserItem"]:last-child a`).href,
-        );
+        const url = new URL(video.url);
         const options = { depth: false, incognito: false };
 
         const file = await extract(url, options);
@@ -39,16 +39,16 @@ describe("Scraper: Arte", function () {
     });
 
     it("should return german video URL", async function () {
-        // Récupérer l'URL d'une vidéo sur la page d'accueil.
-        const response = await fetch("https://www.arte.tv/de/");
-        const text = await response.text();
-        const doc = new DOMParser().parseFromString(text, "text/html");
+        // Récupérer l'URL d'une vidéo parmi les vidéos les plus vues.
+        const response = await fetch("https://www.arte.tv/api/rproxy/emac/v3" +
+                                                 "/de/web/data/VIDEO_LISTING/" +
+                                  "?videoType=MOST_VIEWED&authorizedAreas=ALL");
+        const json = await response.json();
+        // Garder les éléments pointant vers une seule vidéos (et non vers une
+        // liste de vidéos).
+        const video = json.data.find((d) => !d.kind.isCollection);
 
-        // Récupérer le dernier de la liste pour ne pas prendre des vidéos qui
-        // ne sont pas encore disponibles.
-        const url = new URL(
-            doc.querySelector(`[data-testid="teaserItem"]:last-child a`).href,
-        );
+        const url = new URL(video.url);
         const options = { depth: false, incognito: false };
 
         const file = await extract(url, options);
