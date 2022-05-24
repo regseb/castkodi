862dd2b3bd6b1cf395246052a6c2e9a5cdcc3f61
Sébastien Règne <regseb@gmail.com>
2022-05-24 10:55:16 +0200

    test: Actualiser les tests de Jamendo.

--- test/integration/scraper/jamendo.js
+++ test/integration/scraper/jamendo.js
@@ -16,8 +16,8 @@ describe("Scraper: Jamendo", function () {
         const options = { depth: false, incognito: false };
 
         const file = await extract(url, options);
-        assert.ok(file?.startsWith("https://mp3l.jamendo.com/?trackid=3431" +
-                                                                "&format=mp31"),
+        assert.ok(file?.startsWith("https://prod-1.storage.jamendo.com/" +
+                                                   "?trackid=3431&format=mp31"),
                   `"${file}"?.startsWith(...)`);
     });
 
@@ -28,8 +28,8 @@ describe("Scraper: Jamendo", function () {
         const options = { depth: false, incognito: false };
 
         const file = await extract(url, options);
-        assert.ok(file?.startsWith("https://mp3l.jamendo.com/?trackid=33454" +
-                                                                "&format=mp31"),
+        assert.ok(file?.startsWith("https://prod-1.storage.jamendo.com/" +
+                                                  "?trackid=33454&format=mp31"),
                   `"${file}"?.startsWith(...)`);
     });
 });
