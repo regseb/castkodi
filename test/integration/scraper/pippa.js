6b657a851e4d116a217aef15abf51eb232d83698
Sébastien Règne <regseb@gmail.com>
2022-07-14 14:43:27 +0200

    test: Actualiser les tests de Pippa.

--- test/integration/scraper/pippa.js
+++ test/integration/scraper/pippa.js
@@ -2,14 +2,6 @@ import assert from "node:assert";
 import { extract } from "../../../src/core/scrapers.js";
 
 describe("Scraper: Pippa", function () {
-    it("should return URL when it's not an audio", async function () {
-        const url = new URL("https://shows.pippa.io/studio-404/");
-        const options = { depth: false, incognito: false };
-
-        const file = await extract(url, options);
-        assert.strictEqual(file, url.href);
-    });
-
     it("should return audio URL [opengraph]", async function () {
         const url = new URL("https://shows.pippa.io/cdanslair/episodes" +
                                "/5-decembre-la-greve-qui-fait-peur-22-11-2019");
