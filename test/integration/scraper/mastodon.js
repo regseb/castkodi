4a4aac9bf97e91644ed389b67373dea0c4112fc2
Sébastien Règne <regseb@gmail.com>
2024-06-22 11:24:36 +0200

    test: Actualiser les tests de Mastodon.

--- test/integration/scraper/mastodon.js
+++ test/integration/scraper/mastodon.js
@@ -9,16 +9,14 @@ import { extract } from "../../../src/core/scrapers.js";
 
 describe("Scraper: Mastodon", function () {
     it("should return video URL [opengraph]", async function () {
-        const url = new URL(
-            "https://mastodon.social/@Mi_NumEco_Gouv/108469304023929478",
-        );
+        const url = new URL("https://mamot.fr/@davduf/112586386594480705");
         const context = { depth: false, incognito: false };
 
         const file = await extract(url, context);
         assert.equal(
             file,
-            "https://files.mastodon.social/media_attachments/files/108/469" +
-                "/303/433/550/677/original/811ce06c46f6638f.mp4",
+            "https://static.mamot.fr/media_attachments/files/112/586/378/979" +
+                "/928/374/original/a5230271b22e9032.mp4",
         );
     });
 });
